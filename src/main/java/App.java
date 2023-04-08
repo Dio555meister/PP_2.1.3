840b4be22b22932ad3d071fb078a857c0a67cba7
Oleg <dio555meister@gmail.com>
2023-04-08 19:29:46 +0300

    PP_2.1.3
    PP_2.1.3

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+
+
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("cat1 == cat2 : " + (cat1==cat2));
+        System.out.println("bean1 == bean2 : " + (bean1 == bean1));
+
+
+    }
+}
